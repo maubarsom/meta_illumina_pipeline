e0e8027d45f3a1bf472ca6000906b7f315e2c11d
maubar <mauriciobarr@gmail.com>
2015-05-25 10:45:40 +0200

    Bug fix, wrong variable

--- scripts/plot_kmer_histogram.R
+++ scripts/plot_kmer_histogram.R
@@ -6,6 +6,6 @@ library(ggplot2)
 if(length(args) == 2){
 	kmers <- read.table(args[1],header=F,sep=' ',col.names=c("kmer_freq","count"))
 	pdf(args[2])
-	ggplot(kmer, aes(x=kmer_freq, y=count)) + geom_line() + xlim(c(0,100))
+	ggplot(kmers, aes(x=kmer_freq, y=count)) + geom_line() + xlim(c(0,100))
 	dev.off()
 }
