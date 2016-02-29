189275f1721d564491da3b863c639108a8f2ccdf
Mauricio Barrientos-Somarribas <mauricio.barrientos@ki.se>
2016-02-29 10:31:19 +0100

    Fix hmmscan scripts to deal with new folder/file structure

--- scripts/run_tax_hmmscan.sh
+++ scripts/run_tax_hmmscan.sh
@@ -4,25 +4,25 @@ set -euo pipefail
 module load hmmer
 module load emboss
 
-sample_name=$(basename assembly/*_allctgs.fa _allctgs.fa)
+sample_name=$(basename 4_assembly/*_contigs.fa _contigs.fa)
 threads=16
 #First param is number of procs
 if [ -z $0 ]; then threads=$0; fi
 
 echo "Processing ${sample_name} with ${threads} procs"
 
-mkdir -p tax_assign/hmmscan/input
+mkdir -p 5_tax_diamond/hmmscan/input
 
-if [ ! -e tax_assign/hmmscan/input/${sample_name}_diamond_nohits.fa ];
+if [ ! -e 5_tax_diamond/hmmscan/input/${sample_name}_diamond_nohits.fa ];
 then
-cat <(python scripts/extract_diamond_nohits.py assembly/*_allctgs.fa tax_assign/diamond/*_allctgs_diamond_nr.sam.gz) \
-	<(python scripts/extract_diamond_nohits.py assembly/*_se.fa tax_assign/diamond/*_se_diamond_nr.sam.gz)  | seqtk seq -L 400 - > tax_assign/hmmscan/input/${sample_name}_diamond_nohits.fa
+cat <(python scripts/extract_diamond_nohits.py 4_assembly/*_contigs.fa 5_tax_diamond/diamond/*_contigs_diamond_nr.sam.gz) \
+	<(python scripts/extract_diamond_nohits.py 4_assembly/*_merged.fa 5_tax_diamond/diamond/*_merged_diamond_nr.sam.gz)  | seqtk seq -L 350 - > 5_tax_diamond/hmmscan/input/${sample_name}_diamond_nohits.fa
 fi
 
-if [[ -s tax_assign/hmmscan/input/${sample_name}_diamond_nohits.fa ]];
+if [[ -s 5_tax_diamond/hmmscan/input/${sample_name}_diamond_nohits.fa ]];
 then
-cd tax_assign/hmmscan
+cd 5_tax_diamond/hmmscan
 make -rf ../../steps/tax_hmmscan.mak SAMPLE=${sample_name} threads=${threads} in_fasta=input/${sample_name}_diamond_nohits.fa
 else
-echo "File tax_assign/hmmscan/input/${sample_name}_diamond_nohits.fa is empty!" >&2
+echo "File 5_tax_diamond/hmmscan/input/${sample_name}_diamond_nohits.fa is empty!" >&2
 fi
