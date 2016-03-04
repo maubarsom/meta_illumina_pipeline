4d5ae684f5779d7a027b2cd8738513ac904de1df
Mauricio Barrientos-Somarribas <mauricio.barrientos@ki.se>
2016-03-04 17:54:51 +0100

    Add script to remove tmp files from the pipeline

---
+++ scripts/clean_tmp_files.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+if [ -z pipeline_stats.ipynb ];
+then
+	echo "You should run the pipeline stats before cleaning the tmp files"
+	exit 1
+fi
+
+#Remove tmp folder
+rm -r tmp/
+
+#Remove temporary qf files
+find 2_qf -name "*.fq.gz" -delete
+
+#Remove
+rm 3_hostfiltering/*.fq
+
+#Remove assembler-specific folders
+rm -r 4_assembly/fermi
+rm -r 4_assembly/megahit
+rm -r 4_assembly/spades
+
+#Remove tmp mapping files to determine non-incorporated reads
+rm 4_assembly/singletons/*.sam
