400207ef6844754389b526ad450ea863c17e7c66
maubar <mauriciobarr@gmail.com>
2016-08-08 18:09:02 +0200

    Create a wrapper to run the preproc pipeline and store logs with sample_id

---
+++ batch_processing/preprocess_sample.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -euo verbose -o pipefail
+
+SAMPLE_ID=$1
+READS_DIR=$2
+SCRIPTS_DIR=/labcommon/viral_discovery/meta_illumina_pipeline/batch_processing
+
+OLD_PATH=${PATH}
+export PATH=${SCRIPTS_DIR}:${PATH}
+
+make -j 3 -f ${SCRIPTS_DIR}/Makefile \
+			sample_id=${SAMPLE_ID} \
+			reads=${READS_DIR}
+
+export PATH=${OLD_PATH}
