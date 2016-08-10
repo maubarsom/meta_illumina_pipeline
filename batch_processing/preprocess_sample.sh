92b524ecf4497306d2628e714b63e39388a57cc4
maubar <mauriciobarr@gmail.com>
2016-08-10 17:30:11 +0200

    Add calculation of checksums for output files and cleans the tmpfs memory

--- batch_processing/preprocess_sample.sh
+++ batch_processing/preprocess_sample.sh
@@ -12,4 +12,14 @@ make -j 3 -f ${SCRIPTS_DIR}/Makefile \
 			sample_id=${SAMPLE_ID} \
 			read_folder=${READS_DIR}
 
+#Calculate checksum of output files
+for x in $(ls *.fq.gz);
+do
+	md5sum ${x} > ${x}.md5
+done
+
+#Clean tmp folder if everything worked alright
+make -f ${SCRIPTS_DIR}/Makefile sample_id=${SAMPLE_ID} clean
+
+#Restore old path
 export PATH=${OLD_PATH}
