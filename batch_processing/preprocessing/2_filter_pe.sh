cbf98b9a85797ebc3deaa4b1a32c5a49cab6f341
Mauricio Barrientos-Somarribas <mauricio.barrientos@ki.se>
2018-12-07 13:49:46 +0100

    Update preprocessing to output separate paired-end files and use new samtools (v 1.9) features

--- batch_processing/preprocessing/2_filter_pe.sh
+++ batch_processing/preprocessing/2_filter_pe.sh
@@ -7,10 +7,11 @@ BOWTIE_DB=/labcommon/db/bowtie2/grch38
 SAMPLE_ID=$1
 IN_DIR=$2
 
-OUT_FILE=${SAMPLE_ID}_pe.fq.gz
+OUT_FILE_R1=${SAMPLE_ID}_1.fq.gz
+OUT_FILE_R2=${SAMPLE_ID}_2.fq.gz
 
-if [ -e ${OUT_FILE} ]; then
-	echo "File ${OUT_FILE} already exists. Delete file to regenerate."
+if [ -s ${OUT_FILE_R1} ] || [ -s ${OUT_FILE_R2} ]; then
+	echo "File ${OUT_FILE_R1} and/or ${OUT_FILE_R2} already exist. Delete file(s) to regenerate."
 	exit 0;
 fi
 
@@ -26,7 +27,9 @@ cutadapt --cut=3 -g ^GCCGGAGCTCTGCAGATATC -g ^GGAGCTCTGCAGATATC --no-indels --er
 cutadapt --cut=3 -g ^GCCGGAGCTCTGCAGATATC -g ^GGAGCTCTGCAGATATC --no-indels --error-rate=0.1 -f 'fastq' \
 	-o R2_PIPE ${IN_DIR}/*_val_2.fq.gz 2>&1 > log/${SAMPLE_ID}_overhangs_R2.log &
 
-bowtie2 --local --very-sensitive-local -t -p 12 -x ${BOWTIE_DB} -1 R1_PIPE -2 R2_PIPE | tee >(samtools view -hSb - | samtools flagstat - > log/${SAMPLE_ID}_bowtie_pe.flagstat ) | samtools view -hSb -f12 -F256 - | samtools fastq - | gzip > ${OUT_FILE}
+bowtie2 --local --very-sensitive-local -t -p 12 -x ${BOWTIE_DB} -1 R1_PIPE -2 R2_PIPE | \
+	tee >(samtools view -hSb - | samtools flagstat - > log/${SAMPLE_ID}_bowtie_pe.flagstat ) | \
+	samtools view -hSb -f12 -F256 - | samtools fastq -1 ${OUT_FILE_R1} -2 ${OUT_FILE_R2}
 
 #Remove named PIPEs
 rm R1_PIPE R2_PIPE
--- batch_processing/preprocessing/2_filter_unpaired.sh
+++ batch_processing/preprocessing/2_filter_unpaired.sh
@@ -27,7 +27,9 @@ cat ${IN_DIR}/*_unpaired_2.fq.gz >> ${IN_DIR}/*_unpaired_1.fq.gz && rm ${IN_DIR}
 cutadapt --cut=3 -g ^GCCGGAGCTCTGCAGATATC -g ^GGAGCTCTGCAGATATC --no-indels --error-rate=0.1 \
 	-o SE_PIPE ${IN_DIR}/*_unpaired_1.fq.gz 2>&1 > log/${SAMPLE_ID}_overhangs_unpaired.log &
 
-bowtie2 --local --very-sensitive-local -t -p 8 -x ${BOWTIE_DB} -U SE_PIPE | tee >(samtools view -hSb - | samtools flagstat - > log/${SAMPLE_ID}_bowtie_unpaired.flagstat ) | samtools view -hSb -f4 -F256 - | samtools fastq - | gzip > ${OUT_FILE}
+bowtie2 --local --very-sensitive-local -t -p 8 -x ${BOWTIE_DB} -U SE_PIPE | \
+	tee >(samtools view -hSb - | samtools flagstat - > log/${SAMPLE_ID}_bowtie_unpaired.flagstat ) | \
+	samtools view -hSb -f4 -F256 - | samtools fastq - | gzip > ${OUT_FILE}
 
 #Remove named PIPEs
 rm SE_PIPE
